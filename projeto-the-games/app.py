07696a37a355627943028fad717d15660d06ae3f
gu1lh3rme-rodrigues <guilherme.robles@etec.sp.gov.br>
2025-02-21 11:54:26 -0300

    @gu1lh3rme-rodrigues

--- projeto-the-games/app.py
+++ projeto-the-games/app.py
@@ -1,20 +1,37 @@
 # Comentário em python
 # Importando o pacote do Flask
-from flask import Flask
+from flask import Flask, render_template
 
 # Carregando o Flask na Váriavel app
-app = Flask(__name__)
+app = Flask(__name__, template_folder='views')
 
-#Criando a rota principal do site
-@app.route ('/')
-#Criando Função no Python
+# Criando a rota principal do site
+
+
+@app.route('/')
+# Criando Função no Python
 def home():
-    return '<h1>Meu primeiro site em Flask. Seja bem-vindo!<br><br>É isso apenas</h1>'
+    return render_template('index.html')
 
-# #atividade do dia 
-# @app.route('/')
-# def games():
-#     return '<h1>Seja bem vindo a página de Games</h1>'
+# atividade do dia
+
+
+@app.route('/games')
+# View function - Função de visualização
+def games():
+    titulo = 'Portal'
+    ano = 2007
+    categoria = 'Puzzle, Singleplayer'
+    jogadores = ['Black.Buterfly', 'Enderboy_404']
+    jogos = ['Portal 2', 'Team Fortess 2', 'CS-GO', 'Half-Life', 'Dota', 'Day of Defeat:Source', 'Left 4 Dead']
+    return render_template('games.html',
+                           titulo = titulo,
+                           ano = ano,
+                           categoria = categoria,
+                           jogadores = jogadores,
+                           jogos = jogos)
+
+
 if __name__ == '__main__':
-    #Rodando o Servidor do Localhost, na porta 5000
-    app.run(host='localhost', port = 5000, debug=True)
+    # Rodando o Servidor do Localhost, na porta 5000
+    app.run(host='localhost', port=5000, debug=True)
